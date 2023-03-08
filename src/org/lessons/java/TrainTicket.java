9e6254e5730cd9f9f759d9867c6ac356e9978553
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-03-08 14:50:28 +0100

    finished without bonus

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -17,17 +17,32 @@ public class TrainTicket {
     Per chiedere informazioni all’utente sarà necessario usare la classe java.util.Scanner, come visto in classe stamattina.
     Bonus: formattare il risultato con un numero limitato di decimali utilizzando la classe DecimalFormat*/
 
+        //Init
         int km;
         int age;
         double rate = 0.21;
+        double underAgeDiscount = 20.0;
+        double seniorDiscount = 40.0;
 
         Scanner scan = new Scanner(System.in);
+        //Get data from user and store it
         System.out.println("How many kilometers is your journey?");
         km = Integer.parseInt(scan.nextLine());
         System.out.println("How old are you?");
         age = Integer.parseInt(scan.nextLine());
 
+        double totalPrice = rate * km;
+
+
+        if(age < 18){
+            totalPrice-= totalPrice * underAgeDiscount / 100;
+        }
+        else if(age > 65){
+            totalPrice-= totalPrice * seniorDiscount / 100;
+        }
+
+        //Final output
         System.out.println("You are " + age + " years old and you want to travel " + km + "Km. The price of your " +
-                "ticket is " + (rate * km) + " Euros");
+                "ticket is " + totalPrice + " Euros");
     }
 }
