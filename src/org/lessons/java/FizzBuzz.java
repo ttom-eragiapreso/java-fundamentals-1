a2187f65fd0e68d4bf9dc709ac60d95f2568a3d4
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-03-08 15:18:49 +0100

    finished without bonus

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -2,6 +2,29 @@ package org.lessons.java;
 
 public class FizzBuzz {
     public static void main(String[] args) {
+    /*
+    * Esercizio 2: FizzBuzz
+    Consegna: Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz”
+    * al posto del numero e per i multipli di 5 stampi “Buzz”.
+    * Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+    Esempio: 1 2 Fizz 4 Buzz Fizz 7 8 Fizz……
+    Bonus: invece che 100, chiedere all’utente quanti numeri vuole stampare
+    * (il valore inserito dall’utente deve essere maggiore di 0 e minore di 1000)*/
 
+
+        for (int i = 1; i <= 100; i++) {
+            String message = Integer.toString(i);
+
+            if(i % 15 == 0){
+                message = "FizzBuzz";
+            }
+            else if(i % 5 == 0){
+                message = "Buzz";
+            }
+            else if(i % 3 == 0){
+                message = "Fizz";
+            }
+            System.out.println(message);
+        }
     }
 }
